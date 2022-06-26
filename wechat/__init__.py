74d9cdc30104688c1ffa44fe5204d99b3a2d5a87
ItGarbager <linxuzhao2018@163.com>
2022-06-26 16:53:39 +0800

    wechat 功能调整，使用单例

--- wechat/__init__.py
+++ wechat/__init__.py
@@ -1,9 +1,14 @@
+import asyncio
 import logging
 import time
+import traceback
 
 from WechatPCAPI import WechatPCAPI
 
-logging.basicConfig(level=logging.INFO)
+from wechat.config import START_TIME
+from wechat.monitor.logger import logger
+from wechat.monitor.message import handle_event
+from wechat.utils import get_friends
 
 
 # 单例模式
@@ -31,8 +36,8 @@ class WxINFO:
 
 @singleton
 class WX:
-    def __init__(self, on_message):
-        self.wx = WechatPCAPI(on_message=on_message, log=logging)
+    def __init__(self):
+        self.wx = WechatPCAPI(on_message=self._on_message, log=logger)
 
     def start(self):
         self.wx.start_wechat(block=True)
@@ -45,3 +50,41 @@ class WX:
 
     def send_text(self, *args, **kwargs):
         self.wx.send_text(*args, **kwargs)
+
+    def _on_message(self, message):
+        """
+        这是消息回调函数，所有的返回消息都在这里接收，建议异步处理，防止阻塞
+        :param message: 回调消息
+        :return:
+        """
+        try:
+            res = get_friends(message, WXFriend=WXFriend)
+
+            # 判断是否为通讯录消息
+            if res:
+                data, chat_type, group, user, msg = res
+
+                # 解析当前消息收发状态
+                try:
+                    send_or_recv = not bool(eval(data.get('send_or_recv', '').split('+', 1)[0]))
+                except:
+                    send_or_recv = False
+
+                # 判断为收取消息并时间大于启动时间才会进行回复
+                if send_or_recv and data.get('time') >= START_TIME:
+                    logger.info('message: %s' % message)
+                    # 异步启动当前注册的事件，插件目录 wechat/plugins/
+                    asyncio.run(handle_event(Message(data, chat_type, group, user, msg, self)))
+
+        except Exception:
+            logger.info('on_message monitor failed %s' % traceback.print_exc())
+
+
+class Message:
+    def __init__(self, data, chat_type, group, user, msg, wx: WX):
+        self.data = data
+        self.chat_type = chat_type
+        self.group = group
+        self.user = user
+        self.msg = msg
+        self.wx = wx
--- wechat/utils.py
+++ wechat/utils.py
@@ -1,47 +1,39 @@
-import logging
-
 import warnings
 
-from wechat import WXFriend, WX
-
 warnings.filterwarnings('ignore')
-logging.basicConfig(level=logging.INFO)
 
 
-# 这是消息回调函数，所有的返回消息都在这里接收，建议异步处理，防止阻塞
-def on_message(message):
-    # 回调的数据及其类型
-    data = message.get('data')
+def get_friends(message, WXFriend):
+    """
+    获取通讯录信息
+    :param message: 回调消息
+    :return: 空 | 格式化后的用户信息
+    """
+    # message dict
     msg_type = message.get('type')
-    try:
-        # 填写监听回调逻辑
-        if msg_type:
+    data = message.get('data')
+    # 处理业务逻辑
 
-            if msg_type.startswith('friend::'):  # 通讯录类型
-                friend_type = msg_type.rsplit('::', 1)[-1]
-                if friend_type == 'person':
-                    _id = 'wx'
-                    _name = 'wx_nick'
-                else:
-                    _id = friend_type
-                    _name = friend_type + '_'
-                if data:
-                    _id = data.get('%s_id' % _id)
-                    _name = data.get('%sname' % _name)
-                    remark_name = data.get('remark_name')
+    if msg_type:
 
-                    # 将通讯录信息传入初始化结构中
-                    getattr(WXFriend, friend_type)[_id] = {
-                        'name': _name,
-                        'type': friend_type,
-                        'remark_name': remark_name
-                    }
-            elif msg_type.startswith('msg::'):  # 消息类型
-                # 处理业务逻辑
-                pass
-    except:
-        pass
+        if msg_type.startswith('friend::'):  # 通讯录类型
+            friend_type = msg_type.rsplit('::', 1)[-1]
+            if friend_type == 'person':
+                _id = 'wx'
+                _name = 'wx_nick'
+            else:
+                _id = friend_type
+                _name = friend_type + '_'
+            if data:
+                _id = data.get('%s_id' % _id)
+                _name = data.get('%sname' % _name)
+                remark_name = data.get('remark_name')
+                getattr(WXFriend, friend_type)[_id] = {'name': _name, 'type': friend_type,
+                                                       'remark_name': remark_name}
+        else:
+            chat_type = msg_type.rsplit('::', 1)[-1]
+            group = data.get('from_chatroom_wxid')
+            user = data.get('from_member_wxid', data.get('from_wxid'))
+            msg = data.get('msg')
 
-
-# 实例化的微信对象
-wx = WX(on_message=on_message)
+            return data, chat_type, group, user, msg
