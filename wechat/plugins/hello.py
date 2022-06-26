bf26412bb3b67bfeaccab2267d9ff5eb8403fa02
ItGarbager <linxuzhao2018@163.com>
2022-06-26 16:51:35 +0800

    自定义插件

---
+++ wechat/plugins/hello.py
@@ -0,0 +1,23 @@
+from wechat.monitor.plugins.on import on_keyword
+from wechat import Message
+# priority 为优先级，数值越低优先级越高，block 是否阻断消息继续传递，默认 True，为 False 时还需继续传递至下一层事件处理
+hello = on_keyword(keywords={'hello', '你好', '早'}, priority=2, block=True)
+
+
+@hello.handle()
+async def hello(message: Message):
+    if message.group:
+        message.wx.send_text(message.group, '你好')
+    else:
+        message.wx.send_text(message.user, 'hello')
+
+
+hello2 = on_keyword(keywords={'hello', '你好', '早'}, priority=3)
+
+
+@hello2.handle()
+async def hello2(message: Message):
+    if message.group:
+        message.wx.send_text(message.group, '你好2')
+    else:
+        message.wx.send_text(message.user, 'hello2')
---
+++ wechat/plugins/scheduler.py
@@ -0,0 +1,38 @@
+from re import split
+
+from wechat import Message
+from wechat.monitor.plugins import on_command
+from wechat.tasks.schedulers import add_task
+
+schedule = on_command(cmd='添加定时', aliases={'添加任务', '添加定时任务'})
+
+task_map_dict = {
+    '发送文本消息': 'send_text',
+    'send_text': 'send_text'
+}
+
+
+@schedule.handle()
+async def _(message: Message):
+    command = message.data.get('command')
+    if command:
+        prefix = command.get('prefix')
+        others = command.get('others')
+        friend = message.group or message.user
+        if others and len(others) == 4:
+            call, date, send_friend, msg = others
+            call = task_map_dict.get(call)
+            if call:
+                if ':' in date and date.replace(':', '').isdigit():
+                    hour, minute = split(r'[:：]', date)
+                    message.wx.send_text(friend, '定时任务添加成功')
+                    if send_friend == '-':
+                        send_friend = friend
+                    add_task(task_map_dict[call], hour, minute, *(send_friend, msg))
+                else:
+                    message.wx.send_text(friend, '时间格式异常')
+            else:
+                message.wx.send_text(friend, '任务类型不存在')
+        else:
+            message.wx.send_text(friend, '命令格式错误，格式示例:\n'
+                                         f'{prefix} 发送文本消息(添加任务类型) 10:10(时间，时:分) xxxxx_yyyy(通讯录id) 发送消息')
