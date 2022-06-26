577b5244bf542b31628d3515b96e91ea380055ab
ItGarbager <linxuzhao2018@163.com>
2022-06-26 16:45:49 +0800

    配置使用 tornado 启动 flask

--- web/__init__.py
+++ web/__init__.py
@@ -1,16 +1,26 @@
+import asyncio
+
 from flask import Flask
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from tornado.wsgi import WSGIContainer
+
 from web.app.routers import wechat_app
 
 
 class Application:
     app = Flask(__name__)
 
-    def __init__(self):
+    def __init__(self, logger=None):
         self.__load_routers()
+        self.logger = logger
 
     def __load_routers(self):
         # 加载所有的二级路由APP
         self.app.register_blueprint(blueprint=wechat_app)
 
-    def start(self, host='127.0.0.1', port=5741, debug=False):
-        self.app.run(host=host, port=port, debug=debug)
+    def start(self, port=5741):
+        asyncio.set_event_loop(asyncio.new_event_loop())
+        http_server = HTTPServer(WSGIContainer(self.app))
+        http_server.listen(port)
+        IOLoop.instance().start()
