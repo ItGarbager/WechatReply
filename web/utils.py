bf614c0b91421c682dbd0c94d2e7535947070c02
ItGarbager <linxuzhao2018@163.com>
2022-06-26 16:48:12 +0800

    json 返回时间戳修改

---
+++ web/utils.py
@@ -0,0 +1,35 @@
+import time
+
+from flask import jsonify
+
+STATUS_CODE_DICT = {
+    200: 'Success',
+    400: 'Bad Request',
+    401: 'Unauthorized',
+    403: 'Forbidden',
+    404: 'Not Found',
+    405: 'Method Not Allowed',
+    408: 'Time Out',
+    500: 'Internal Server Error',
+    502: 'Bad GateWay',
+    503: 'Service Unavailable'
+
+}
+
+
+def global_response(data=None, status=None, msg=None):
+    if msg is None:
+        if not status:
+            status = 200 if data else 404
+        msg = STATUS_CODE_DICT.get(status, STATUS_CODE_DICT[status])
+    else:
+        if not status:
+            status = 200
+
+    data = {
+        'data': data,
+        'msg': msg,
+        'status': status,
+        'timestamp': int(time.time())
+    }
+    return jsonify(data), status
