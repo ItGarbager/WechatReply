6f97fbccbf4703850734bba3761897c5e5804b3d
ItGarbager <linxuzhao2018@163.com>
2022-06-27 22:31:07 +0800

    默认内置插件

---
+++ monitor/plugins/echo.py
@@ -0,0 +1,10 @@
+from ..plugin import on_full_match  # 先导入一个响应器
+
+# 全文匹配响应器， priority 越低优先级越高, block 是否阻断向低优先级传递, 默认为 True
+ping = on_full_match(msg=('ping',), priority=1, block=True)
+
+
+@ping.handle()
+async def _(message):
+    friend = message.group or message.user
+    message.wx.send_text(friend, 'pong!')
